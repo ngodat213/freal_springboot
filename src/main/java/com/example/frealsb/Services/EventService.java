e8f55cd455c4272c3d1d06b440d8c1706a435a3e
Hydra Coder <96688421+ngodat213@users.noreply.github.com>
2024-06-16 23:29:51 +0700

    add crud EventService

--- src/main/java/com/example/frealsb/Services/EventService.java
+++ src/main/java/com/example/frealsb/Services/EventService.java
@@ -1,8 +1,47 @@
 package com.example.frealsb.Services;
 
+import com.example.frealsb.Entities.Event;
+import com.example.frealsb.Repositories.EventRepository;
+import com.example.frealsb.RequestEntities.RequestEvent;
 import com.example.frealsb.Services.Interface.IEventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class EventService implements IEventService {
+    @Autowired
+    private EventRepository eventRepository;
+
+    @Override
+    public List<Event> getEvents(String s, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return eventRepository.findAllBy(s, pageable);
+    }
+
+    @Override
+    public Event getEvent(String id) {
+        return eventRepository.findById(id).get();
+    }
+
+    @Override
+    public Event addEvent(RequestEvent req) {
+        return eventRepository.save(req.toAddData());
+    }
+
+    @Override
+    public Event saveEvent(RequestEvent req) {
+        return eventRepository.save(req.toUpdateData());
+    }
+
+    @Override
+    public Event deleteEvent(String id) {
+        Event event = getEvent(id);
+        event.setDeletedAt(new Date());
+        return eventRepository.save(event);
+    }
 }
